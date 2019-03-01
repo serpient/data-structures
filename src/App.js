288367de33cef8ce39a78bc045b6e95c1a425cab
serpient <littlegangwolf@gmail.com>
2019-03-01 00:11:51 -0800

    remove unused logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import LinkedListVisualizer from 'visualizer/LinkedList';
+import React, { Component } from "react";
+import "./App.scss";
+import LinkedListVisualizer from "visualizer/LinkedList";
 
 class App extends Component {
   render() {
